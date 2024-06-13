0d2ab7ed23aebbc5520511f86952a79ef99cb94d
Zaidan07 <zaidanxgaming@gmail.com>
2024-06-13 08:22:56 +0700

    progress

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='absolute z-30 w-full'>
+        <div className='max-w-6xl px-4 mx-auto sm:*:px-6'>
+            <div className='flex items-center justify-between h-16 md:h-20'>
+                <a href="/"></a>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
